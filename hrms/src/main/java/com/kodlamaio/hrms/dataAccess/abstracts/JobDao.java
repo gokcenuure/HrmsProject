a2dc81dc213e42117ac867775133d9195be1c49d
ASUS <ASUS@LAPTOP-2FHP68LB>
2021-05-22 15:29:44 +0300

    HrmsProject

---
+++ hrms/src/main/java/com/kodlamaio/hrms/dataAccess/abstracts/JobDao.java
@@ -0,0 +1,11 @@
+package com.kodlamaio.hrms.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kodlamaio.hrms.entities.concretes.Job;
+
+
+public interface JobDao extends JpaRepository<Job,Integer> {
+
+
+}
